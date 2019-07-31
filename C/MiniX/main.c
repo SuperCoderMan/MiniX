d32e5c8dcbd798c963f317ebba7e079d4ea16d8f
SuperCoderMan <2624187656@qq.com>
2019-07-31 09:47:23 +0800

    Create project for C programming.

---
+++ C/MiniX/main.c
@@ -0,0 +1,24 @@
+/**
+ * @file	    :	main.c
+ * @author		:	Kevin
+ * @version		:	0.0.1
+ * @since		:	2019年7月30日18:34:47
+ * @description	:	Hello World
+ */
+
+#include <stdio.h>
+
+/**
+ * @function	:	main
+ * @author		:	Kevin
+ * @since		:	2019年7月30日18:36:31
+ * @description	:	main函数
+ * @parameter	:	none
+ * @return		:	0
+ */
+int main(int argc, char *argv[])
+{
+	printf("Hello World!\n");
+
+	return 0;
+}
